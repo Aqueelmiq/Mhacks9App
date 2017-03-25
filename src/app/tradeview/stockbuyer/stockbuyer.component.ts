918c92d3236ea411ac9664d909975a79ce0d6fbf
Aditya Parakh <aparakh@hawk.iit.edu>
2017-03-25 16:28:43 -0400

    lol

--- src/app/tradeview/stockbuyer/stockbuyer.component.ts
+++ src/app/tradeview/stockbuyer/stockbuyer.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stockbuyer.component.css']
 })
 export class StockbuyerComponent implements OnInit {
-
-  constructor() { }
+  quantity: number;
+  currentPrice: number;
+  mathh: string;
+  buying: boolean;
+  toggleText: string;
+  constructor() {
+    this.toggleText = "Buy Stock"
+    this.buying = true;
+    this.quantity = 0;
+    this.currentPrice = 4.3;
+    this.mathh = "$ " + Math.round(this.quantity*this.currentPrice*100)/100;
+  }
 
   ngOnInit() {
   }
 
+  calculatePrice(){
+    this.mathh = "$ " + Math.round(this.quantity*this.currentPrice*100)/100;
+  }
+
+  stock_buy(){
+    this.buying = true;
+    this.toggleText = "Buy Stock"
+  }
+
+  stock_sell(){
+    this.buying = false;
+    this.toggleText = "Sell Stock"
+  }
 }
