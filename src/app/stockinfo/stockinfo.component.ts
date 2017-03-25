c1262dc52f34c63975c69c9342650ac22baf08f5
Aditya Parakh <aparakh@hawk.iit.edu>
2017-03-25 12:40:16 -0400

    Added stockInfo

--- src/app/stockinfo/stockinfo.component.ts
+++ src/app/stockinfo/stockinfo.component.ts
@@ -6,8 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stockinfo.component.css']
 })
 export class StockinfoComponent implements OnInit {
+  memeName:String;
+  currentPrice:String;
+  marketCap: String;
+  opening: String;
+  memeHigh: String;
+  memeLow: String;
 
-  constructor() { }
+  constructor() {
+    this.memeName = "Pepe the frog";
+    this.currentPrice = "$5";
+    this.marketCap = "50 mil";
+    this.opening = "$4";
+    this.memeHigh = "10";
+    this.memeLow = "28";
+  }
 
   ngOnInit() {
   }
