4700f11382152b1d1db43c6ee1b5041a22b8aaa2
Javier Sorribes Camargo <javiersorribes@hotmail.es>
2017-03-25 15:54:56 -0400

    Generalize functionality for any meme`

--- server/searchRetrieve.py
+++ server/searchRetrieve.py
@@ -1,18 +1,20 @@
 import urllib.request
 import re
 
-with urllib.request.urlopen('http://knowyourmeme.com/memes/forever-alone') as w:
-    html = w.read()
-    regexp = r"<a href=\\'\/memes\/forever-alone\\' rel=\\'nofollow\\'>(\d*,?\d*,?\d+)<\/a>"
-    print("regexp:",regexp)
-    #with open("text.txt", 'w') as f:
-     #   f.write(str(html))
-    print(html[:1000])
-    find = re.search(regexp, str(html))
-    if find:
-        print()
-        #print(find.group(0))
-        print(find.group(1))
-        print(find)
-    else:
-        print("Not found")
+def totalSearch(meme):
+    extension = '-'.join(word.lower() for word in meme.split())
+    url = 'http://knowyourmeme.com/memes/{}'.format(extension)
+    regexp = r"<a href=\\'\/memes\/{}\\' rel=\\'nofollow\\'>(\d*,?\d*,?\d+)<\/a>".format(extension)
+
+    with urllib.request.urlopen(url) as w:
+        html = w.read()
+        find = re.search(regexp, str(html))
+        if find:
+            numparts = find.group(1).split(',')
+            return int(''.join(numparts))
+        else:  #error handling can be improved
+            print("Not found")
+            return None
+
+if __name__ == '__main__':  #for debugging purposes
+    print(totalSearch("me gusta"))
