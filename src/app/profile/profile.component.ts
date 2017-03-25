d82cfb21c355ecffbe791e0323e1e4a5cd61b27c
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-03-25 18:31:50 -0400

    Profile page components added

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,22 +8,8 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class ProfileComponent implements OnInit {
 
-  memeName:String;
-  currentPrice:String;
-  marketCap: String;
-  opening: String;
-  memeHigh: String;
-  memeLow: String;
-  profileImg: String;
+  constructor() {
 
-  constructor(public routing:ActivatedRoute) {
-    this.memeName = "Pepe the frog";
-    this.currentPrice = "$5";
-    this.marketCap = "50 mil";
-    this.opening = "$4";
-    this.memeHigh = "10";
-    this.memeLow = "28";
-    this.profileImg = "http://emojipedia-us.s3.amazonaws.com/cache/29/27/29278aa57ced9c9b3861d8f4960e4748.png";
   }
 
   ngOnInit() {
