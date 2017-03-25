6354dd1c2ce68a120bc30a0b483cd32ba41a143e
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-03-25 15:10:31 -0400

    UI for trade complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {AngularFireModule, AuthProviders, AuthMethods} from 'angularfire2';
 import { RouterModule } from '@angular/router';
+import { ChartsModule } from 'ng2-charts';
 
 import { DataService } from './services/dataservice/dataservice.service';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
-import { SearchcomponentComponent } from './searchcomponent/searchcomponent.component';
+import { SearchcomponentComponent } from './main/searchcomponent/searchcomponent.component';
 import { TradeviewComponent } from './tradeview/tradeview.component';
 import { NavComponent } from './nav/nav.component';
-import { GraphComponent } from './graph/graph.component';
-import { StockinfoComponent } from './stockinfo/stockinfo.component';
-import { StockbuyerComponent } from './stockbuyer/stockbuyer.component';
+import { GraphComponent } from './tradeview/graph/graph.component';
+import { StockinfoComponent } from './tradeview/stockinfo/stockinfo.component';
+import { StockbuyerComponent } from './tradeview/stockbuyer/stockbuyer.component';
 import { ProfileComponent } from './profile/profile.component';
 
 export const firebase = {
@@ -63,7 +64,8 @@ const authConfig = {
     RouterModule.forRoot(ROUTES),
     AngularFireModule.initializeApp(firebase, authConfig),
     FormsModule,
-    HttpModule
+    HttpModule,
+    ChartsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
