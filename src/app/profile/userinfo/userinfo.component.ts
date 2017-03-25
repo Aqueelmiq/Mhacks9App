78d0cf2c8ed02b05444f9152d5e3f4fab8d6ab46
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-03-25 18:39:44 -0400

    Profile page components added

---
+++ src/app/profile/userinfo/userinfo.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-userinfo',
+  templateUrl: './userinfo.component.html',
+  styleUrls: ['./userinfo.component.css']
+})
+export class UserinfoComponent implements OnInit {
+
+  memeName:String;
+  currentPrice:String;
+  marketCap: String;
+  opening: String;
+  memeHigh: String;
+  memeLow: String;
+  profileImg: String;
+
+  constructor(public routing:ActivatedRoute) {
+    this.memeName = "Pepe the frog";
+    this.currentPrice = "$5";
+    this.marketCap = "50 mil";
+    this.opening = "$4";
+    this.memeHigh = "10";
+    this.memeLow = "28";
+    this.profileImg = "http://emojipedia-us.s3.amazonaws.com/cache/29/27/29278aa57ced9c9b3861d8f4960e4748.png";
+  }
+
+  ngOnInit() {
+  }
+
+}
