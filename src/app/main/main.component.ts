3617c9ee7327db11d2faa9a083e4b1ce1635e982
Aditya Parakh <aparakh@hawk.iit.edu>
2017-03-25 06:20:48 -0400

    Modal view

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AngularFire} from 'angularfire2';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -7,9 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  email: String;
+  password: String;
+  login: Boolean;
+  btnText: String;
+  toggleText: String;
+  poopypie: String = "lol";
+
+  constructor(public af: AngularFire, private router: Router) {
+    this.email = '';
+    this.password = '';
+    this.login = true;
+    this.btnText = "Login";
+    this.toggleText = "Don't have an account? Sign up";
+    this.af.auth.subscribe((auth) => {
+      console.log(auth);
+      if(auth) {
+        router.navigate(['/home']);
+      }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onToggle() {
+    if(this.login) {
+      this.login = false;
+      this.btnText = 'Sign Up';
+      this.toggleText = "Already Registered? Log in";
+    }
+    else {
+      this.login = true;
+      this.btnText = 'Login';
+      this.toggleText = "Don't have an account? Sign up";
+    }
+  }
+
+  onAuthenticate() {
+    if(this.login) {
+      this.onLogin();
+    }
+    else {
+      this.onSignup();
+    }
+  }
+
+  onLogin() {
+    console.log(this.email);
+    this.af.auth.login({
+      email: '' + this.email,
+      password: '' + this.password,
+    });
+  }
+
+  onSignup() {
+    if(this.password.length < 6) {
+      alert('Password should have at least 6 characters!');
+    }
+    this.af.auth.createUser({
+      email: '' + this.email,
+      password: '' + this.password,
+    });
   }
 
 }
