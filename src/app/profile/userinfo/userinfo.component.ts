40a84ae69871c4702dbbf51469248c336f0ecbd6
Aditya Parakh <aparakh@hawk.iit.edu>
2017-03-26 00:53:03 -0400

    added

--- src/app/profile/userinfo/userinfo.component.ts
+++ src/app/profile/userinfo/userinfo.component.ts
@@ -11,39 +11,57 @@ export class UserinfoComponent implements OnInit {
   items: FirebaseObjectObservable<any[]>;
   url: string;
 
-  memeName:String;
-  currentPrice:String;
-  marketCap: String;
-  opening: String;
-  memeHigh: String;
-  memeLow: String;
-  profileImg: String;
+  userName:string;
+  balance:string;
+  marketCap: string;
+  opening: string;
+  memeHigh: string;
+  memeLow: string;
+  profileImg: string;
+  itom:Object;
 
   constructor(public routing:ActivatedRoute,  public af: AngularFire) {
-    this.memeName = "Pepe the frog";
-    this.currentPrice = "$5";
+    this.userName = "Pepe the frog";
+    this.balance = "$5";
     this.marketCap = "50 mil";
     this.opening = "$4";
     this.memeHigh = "10";
     this.memeLow = "28";
     this.profileImg = "http://emojipedia-us.s3.amazonaws.com/cache/29/27/29278aa57ced9c9b3861d8f4960e4748.png";
+/*
+    this.af.auth.subscribe((auth) => {
+      this.url = "user/" + auth["uid"];
+      console.log(this.url);
+      this.items = this.af.database.object(this.url);
+      this.items.subscribe( item => {
+        this.itom = item;
+        console.log(item);
+      }
+
+      );
+    });
+    */
+  }
+
+  ngOnInit() {
 
     this.af.auth.subscribe((auth) => {
       this.url = "user/" + auth["uid"];
       console.log(this.url);
       this.items = this.af.database.object(this.url);
       this.items.subscribe( item => {
-          console.log(JSON.stringify(item));
-          this.memeName = item["2"].$value,
-            this.currentPrice = item["0"].$value,
-            this.profileImg = item["1"].$value
-      }
-
+          this.itom = item;
+          console.log(item);
+          this.userName = this.itom["name"];
+          console.log(this.itom["name"]);
+          this.balance = this.itom["balance"];
+        console.log(this.itom["balance"]);
+          this.profileImg = this.itom["img"];
+        }
       );
     });
+    console.log
   }
 
-  ngOnInit() {
-  }
 
 }
