c22287e864be4022758033a79018e1904c34b793
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-03-25 21:04:56 -0400

    dsbhbdsu

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const googleTrends = require('google-trends-api');
 
 /* GET api listing. */
-router.get('/', (req, res) =>
-  res.send('api works')
+router.get('/:key', (req, res) =>
+  googleTrends.interestOverTime({keyword: req.params.key}, function(err, results){
+    if(err) console.error('there was an error!', err);
+    else {
+      let data = JSON.parse(results);
+      res.json({name: req.params.key, trends: data});
+    }
+  })
 );
 
 module.exports = router;
