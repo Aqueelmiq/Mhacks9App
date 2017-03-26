6120e8f94d774224e2b6866a01d921b63b2793e9
Aditya Parakh <aparakh@hawk.iit.edu>
2017-03-26 04:59:52 -0400

    ok

--- src/app/tradeview/stockbuyer/stockbuyer.component.ts
+++ src/app/tradeview/stockbuyer/stockbuyer.component.ts
@@ -16,7 +16,9 @@ export class StockbuyerComponent implements OnInit {
   toggleText: string;
   uid: string;
   stocks;
-
+  balance: number;
+  new_bal:number;
+  stock_dic: Object;
   constructor(public af: AngularFire) {
 
     this.toggleText = "Buy Stock"
@@ -26,11 +28,18 @@ export class StockbuyerComponent implements OnInit {
     this.mathh = "$ " + Math.round(this.quantity*this.currentPrice*100)/100;
     this.af.auth.subscribe(auth => {
       this.uid = auth.uid;
-      this.stocks = this.af.database.object('/user/' + auth.uid + "/stocks");
+      this.stocks = this.af.database.object('/user/' + auth.uid);
+      this.stocks.subscribe(item => {
+        this.balance = item["balance"];
+        this.stock_dic = item["stocks"];
+        }
+
+      )
     })
   }
 
   ngOnInit() {
+
   }
 
   calculatePrice(){
@@ -45,12 +54,30 @@ export class StockbuyerComponent implements OnInit {
     let stamp = this.name + "@" + today.getTime();
     x[stamp] = {quantity: this.quantity, date: today.getTime(), pur_price: this.currentPrice, name: this.name};
     this.stocks = this.af.database.object('/user/' + this.uid + "/stocks/" + stamp);
-
-    this.stocks.update(x[stamp]);
+    this.new_bal = this.balance - this.quantity*this.currentPrice;
+    if(this.quantity != 0){
+      this.stocks.update(x[stamp]);
+    }
+    this.stocks = this.af.database.object('/user/' + this.uid );
+    var y = {};
+    y["balance"] = this.new_bal
+    if(this.quantity != 0){
+      this.stocks.update(y);
+    }
+    if(this.new_bal <0){
+      alert("You cannot make that transaction. Not enough money");
+    }
   }
 
   stock_sell(){
     this.buying = false;
     this.toggleText = "Sell Stock"
+    console.log(this.stock_dic);
+    for(let key in this.stock_dic){
+      console.log(key);
+      if(key.search(this.name)){
+        console.log(key);
+      }
+    }
   }
 }
